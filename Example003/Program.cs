d5871b8f65bcf1dabcb82f2eba8c710333653014
Liliya <liliya89fm@mail.ru>
2022-08-05 13:27:42 +0500

    END

---
+++ Example003/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Программа, которая на вход принимает число и выдаёт, является ли число чётным.");
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number%2==0)
+{
+    Console.Write("Введенное число является четным!");
+}
+else
+{
+     Console.Write("Введенное число является нечетным!");
+}
---
+++ Новая папка/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее");
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+    Console.Write("Первое число больше второго " + "(" + firstNumber +" > "+ secondNumber+ ")");
+}
+else 
+{
+    Console.Write("Первое число меньше второго " + "(" +firstNumber+ " < "+ secondNumber+")");
+}
+
